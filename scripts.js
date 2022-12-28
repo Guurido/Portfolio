5f7a2e8aa226b73fc0dcb78dde40050c73fa44c8
Yanis CHATELET <97635006+Guurido@users.noreply.github.com>
2022-12-28 22:21:46 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,18 +2,16 @@ function scrollElement(){
   window.scroll(0, Position(document.getElementById("wai")));
 }
 
-function Position(obj){
-  var currenttop = -150;
-  if (obj.offsetParent){
-   do{
-    currenttop += obj.offsetTop;
-   }while ((obj = obj.offsetParent));
-   return [currenttop];
-  }
-}
-
 function scrollElement2(){
   window.scroll(0, Position(document.getElementById("projets")));
+}
+
+function scrollElement3(){
+  window.scroll(0, Position(document.getElementById("contact")));
+}
+
+function scrollElement0(){
+  window.scroll(0, Position(document.getElementById("accueil")));
 }
 
 function Position(obj){
